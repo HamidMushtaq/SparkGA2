95a161b2edc3d6611757bc34589be351860333cd
hamidmushtaq <h.mushtaq@tudelft.nl>
2017-08-18 16:14:16 +0200

    .

--- runAll.py
+++ runAll.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 #****************************************/
 #	Script:		runAll.py	
-#	Author:		Hamid Mushtaq 
-#	Company:	TU Delft 		
+#	Author:		Hamid Mushtaq  		
 #****************************************/
 from xml.dom import minidom
 import sys
@@ -12,6 +11,9 @@ import time
 logFile = "time.txt"
 configFile = sys.argv[1]
 startingPart = 1
+
+doc = minidom.parse(configFile)
+outputFolder = doc.getElementsByTagName("outputFolder")[0].firstChild.data
 
 if len(sys.argv) > 2: 
 	startingPart = int(sys.argv[2])
@@ -30,6 +32,9 @@ def getElapsedStr(elapsed):
 
 times_list = []
 times_list.append(time.time())
+
+if startingPart == 1:
+	os.system("hadoop fs -rm -r .Trash/*")
 
 iterations = 4 - startingPart
 for i in range(0, iterations):
